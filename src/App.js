e94b6537de93b6c4b3ebd5abc519da6ab4643e50
Mel <huntermelverton@gmail.com>
2018-05-22 17:09:53 -0400

    a bunch of just stuff

--- src/App.js
+++ src/App.js
@@ -1,75 +1,109 @@
 import React, { Component } from 'react';
-import ReactFileReader from 'react-file-reader';
 import './App.css';
 
-const ImagePreViewON = (props) => {
- if (props.imagePreviewUrl) {
-   return (
-     <div className="image-container">
-       <img src={props.imagePreviewUrl} />
-     </div>
-   )
- }
-  return <div></div>;
-}
-class ImageUpload extends React.Component {
+
+let processedFiles = 0;
+let totalFiles = 0;
+
+class ImageUpload extends Component {
   constructor(props) {
     super(props);
+    this.handleImageChange = this.handleImageChange.bind(this);
+    this.dropHandler = this.dropHandler.bind(this);
+    this.scanFile = this.scanFile.bind(this);
+    this.onLoadEndHandler = this.onLoadEndHandler.bind(this);
     this.state = {
+      images: [],
       files: [],
-      file: '',
-      imagePreviewUrl: ''
+      isLoaded: false,
     };
   }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: do something with -> this.state.file
-    console.log('handle uploading-', this.state.file);
-  }
-
   scanFile(file) {
-    var reader = new FileReader();
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let image = new Image();
+      image.src = e.target.result;
+      file.src = image.src;
+      this.onLoadEndHandler();
+    }
+    this.setState(prevState => ({
+      images: [...prevState.images, file]
+    }));
     reader.readAsDataURL(file);
-    console.log(file);
   }
-
   handleImageChange = (e) => {
     e.preventDefault();
     let files = e.target.files;
-    let file = e.target.files[0];
+    totalFiles = files.length;
     if (files && files[0]) {
       Object.keys(files).map((key, index) => {
-        this.setState({
-          files: this.state.files.concat([files[key]]),
-        });
-        scanFile(files[key]);
+        this.setState(prevState => ({
+          files: [...prevState.files, files[key]],
+        }));
+        return this.scanFile(files[key]);
       });
     }
   }
-
+  dropHandler = (e) => {
+    e.preventDefault();
+    let files = e.dataTransfer.files;
+    if (files && files[0]) {
+      Object.keys(files).map((key, index) => {
+        this.setState(prevState => ({
+          files: [...prevState.files, files[key]],
+        }));
+        return this.scanFile(files[key]);
+      });
+    }
+  }
+  onLoadEndHandler() {
+    processedFiles++;
+    if (processedFiles === totalFiles) {
+      this.setState({
+        isLoaded: true
+      })
+    }
+  }
   render() {
-    console.log(this.state);
+    const { isLoaded } =  this.state;
     return (
       <div>
-        <form onSubmit={this._handleSubmit}>
-          <input
-            type="file"
-            onChange={this.handleImageChange} multiple/>
-        </form>
-        <ImagePreViewON imagePreviewUrl={this.state.imagePreviewUrl} />
+        <div className="drop--file" draggable="true" onDrop={this.dropHandler} onDragOver={(e) => e.preventDefault()}>
+          <form onSubmit={this._handleSubmit}>
+            <label className="file--browse" htmlFor="browse--input">Select Images from your computer</label>
+            <input
+              id="browse--input"
+              name="file"
+              type="file"
+              onChange={this.handleImageChange} multiple/>
+          </form>
+        </div>
+          { isLoaded ?
+            <div className="file--preview">
+              {this.state.images.map((image, i) => {
+                return (
+                  <div key={i} className="image--files">
+                    <img key={image.name} alt={image.name} src={image.src} />
+                    <h5>{image.name}</h5>
+                  </div>
+                )
+              })}
+          </div>
+          :
+          <div></div> }
       </div>
+
     )
   }
 }
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <main className="App">
         <h3> React Image Upload </h3>
-        <ImageUpload/>
-      </div>
+        <ImageUpload />
+      </main>
     )
   }
 }
