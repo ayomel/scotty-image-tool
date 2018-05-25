8a761620fcac60665b0698540e544e1eb0fe4e32
Mel <huntermelverton@gmail.com>
2018-05-25 11:44:20 -0400

    updates

--- src/Header.js
+++ src/Header.js
@@ -8,24 +8,23 @@ class Header extends Component {
       <header>
         <div className="header--wrap">
           <div className="header--main--wrap">
-            <a href="/"><img src={logo} className="App-logo" alt="logo" /></a>
+            <span>Logo</span>
             <h1>Scotty CMS</h1>
-          </div>
-          <div className="nav-bar">
-            <nav>
-              <ul>
-                <li><a>All Shows</a></li>
-                <li><a>All Series</a></li>
-                <li><a>All Collection</a></li>
-                <li><a>Home Rows</a></li>
-                <li><a>Home Option</a></li>
-                <li><a>Genres</a></li>
-                <li><a>Manage Free Content</a></li>
-              </ul>
-            </nav>
-          </div>
+        	</div>
+          <nav>
+            <ul>
+              <li><a>All Shows</a></li>
+              <li><a>All Series</a></li>
+              <li><a>All Collection</a></li>
+              <li><a>Home Rows</a></li>
+              <li><a>Home Option</a></li>
+              <li><a>Genres</a></li>
+              <li><a>Manage Free Content</a></li>
+            </ul>
+          </nav>
         </div>
       </header>
+
     )
   }
 }
